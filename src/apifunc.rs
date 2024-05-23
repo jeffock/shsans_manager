7a9db9bba9c7888f88f27b057161681847093832
jeffock <jeffock1656@gmail.com>
2024-05-22 20:12:00 -0500

    API works, except keywords

--- src/apifunc.rs
+++ src/apifunc.rs
@@ -1,8 +1,24 @@
 use serde_json::Value;
 
-#[path = "paperstruct.rs"] mod paperstruct;
-use paperstruct::Paper;
+use crate::paperstruct::Paper;
 
-pub async fn fetch_paper(doi: &str) -> Result<Paper, Error> {
-    Ok(())
+pub async fn fetch_paper(doi: &str) -> Result<Paper, Box<dyn std::error::Error>> {
+    let url = format!("https://api.crossref.org/works/{}", doi);
+    let response = reqwest::get(&url).await?;
+
+    if response.status().is_success() {
+        let json: Value = response.json().await?;
+        let title = json["message"]["title"][0].as_str().unwrap_or("").to_string();
+        let keywords: Vec<String> = json["message"]["subject"]
+            .as_array()
+            .unwrap_or(&vec![])
+            .iter()
+            .filter_map(|v| v.as_str().map(String::from))
+            .collect();
+
+        let paper_out = Paper::new(doi.to_string(), title, keywords);
+        Ok(paper_out)
+    } else {
+        Err(format!("Failed to fetch paper with DOI {}", doi).into())
+    }
 }
